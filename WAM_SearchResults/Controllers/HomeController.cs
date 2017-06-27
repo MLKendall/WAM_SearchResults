2d08e047189df9bcdd9c531677789c2931194a0c
M Kendall <mlkendall93@gmail.com>
2017-06-27 15:25:09 -0400

    Initial Commit

---
+++ WAM_SearchResults/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WAM_SearchResults.Models;
+
+namespace WAM_SearchResults.Controllers
+{
+    public class HomeController : Controller
+    {
+        //db = new DatabaseContext();
+        private List<Employee> Employees = new List<Employee>();
+
+        public HomeController()
+        {
+            Employee temp = new Employee();
+            temp.name = "Amy Smith";
+            temp.title = "CEO";
+            Employees.Add(temp);
+
+            temp = new Employee();
+            temp.name = "John Doe";
+            temp.title = "Vice President";
+            Employees.Add(temp);
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string Search(string userInput)
+        {
+            string result;
+            List<Employee> resultsArray = new List<Employee>();
+
+            if (userInput != "")
+            {
+                foreach (var employee in Employees) 
+                {
+                    if (employee.name.Contains(userInput) || employee.title.Contains(userInput))
+                    {
+                        resultsArray.Add(employee);
+                    }
+
+                }
+            }
+
+            else
+            {
+                resultsArray = Employees; 
+            }
+
+            result = JsonConvert.SerializeObject(resultsArray, Formatting.None,
+                      new JsonSerializerSettings
+                      {
+                          ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                          ContractResolver = new CamelCasePropertyNamesContractResolver()
+                      });
+
+            return result;
+
+            
+        }
+
+    }
+}
---
+++ WAM_SearchResults/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WAM_SearchResults.Models
+{
+    public class Employee
+    {
+        public string name { get; set; }
+        public string title { get; set; }
+    }
+}
